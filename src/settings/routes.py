4017649a1dcfa37a461cf94e389ec0d25271e18f
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2022-10-25 23:21:12 +0300

    added flake8 in ci

--- src/settings/routes.py
+++ src/settings/routes.py
@@ -4,5 +4,5 @@ from src.apps.accounts.api.v1.routes.users import user_router
 from src.apps.accounts.api.v1.routes.auth import auth_router
 
 api_v1_router = APIRouter()
-api_v1_router.include_router(user_router, prefix='/users', tags=['users'])
+api_v1_router.include_router(user_router, prefix='/users')
 api_v1_router.include_router(auth_router, prefix='/accounts')
